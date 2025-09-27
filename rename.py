d3d94a68c28f90dd040b66b46ce4938e1598f7d9
Lenz <lenz@home>
2025-09-27 10:14:04 +0200

    Formatting

--- rename.py
+++ rename.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from itertools import groupby
 import re
 
+
 class Rename:
     """Classe creata per racchiudere tutte le funzioni dei pezzi"""
 
@@ -25,41 +26,40 @@ class Rename:
         self.mRwr = {}
 
     def stats(self):
+        "Stampa le statistiche"
         non_unq = self.find_non_uniques()
         max_code = self.find_max_used_code()
         return f"Files: {len(self.src)} (non-uniques: {len(non_unq)}) - Max code: '{self.to_num(max_code)}' - Rewrite rules: {len(self.mRwr)}"
 
-
     def rewrites(self, mRewrites):
+        "Inserisce regole di riscrittura"
         self.mRwr = mRewrites
 
-    def files_of( self, src ):
-        self.src =  [ (str(f.parent), f.name) for f in Path(src).rglob('*') if f.is_file()]
+    def files_of(self, src):
+        "Legge i files ricorsivamente da una directory"
+        self.src = [(str(f.parent), f.name)
+                    for f in Path(src).rglob('*') if f.is_file()]
 
     def list_of_filenames(self, lFiles):
+        "Per test, carica i files da un array di nomi files"
         def split_fname(filename):
             f = Path(filename)
             return (str(f.parent), f.name)
 
-        self.src = list(map( split_fname, lFiles ))
+        self.src = list(map(split_fname, lFiles))
 
     def keep_good_files(self):
         """
         Tiene solo i file che mi interessamno.
         Li mette tutti in ordine.
         """
-        def is_good( ft ):
+        def is_good(ft):
             (_d, filename) = ft
             return filename.endswith(".mp3") and (not filename.startswith("._"))
-        
-        good_files = filter( is_good, self.src)
-        sorted_files = sorted( good_files )
+
+        good_files = filter(is_good, self.src)
+        sorted_files = sorted(good_files)
         self.src = list(sorted_files)
-        
-
-
-
-
 
     def find_non_uniques(self):
         """
@@ -69,7 +69,8 @@ class Rename:
         di un prefisso numerico.
         """
         risultato = []
-        lista_ordinata = sorted(self.src, key=lambda x: self.remove_numeric_prefix(x[1]))
+        lista_ordinata = sorted(
+            self.src, key=lambda x: self.remove_numeric_prefix(x[1]))
         for _file, gruppo in groupby(lista_ordinata, key=lambda x: self.remove_numeric_prefix(x[1])):
             lista_gruppo = list(gruppo)
 
@@ -78,54 +79,54 @@ class Rename:
                 risultato.append(lista_gruppo)
 
         return risultato
-    
+
     def find_max_used_code(self):
+        "Trova il codice più elevato già in uso."
         maxCode = 0
         for (_d, f) in self.src:
             v = self.get_numeric_prefix(f)
             if v > maxCode:
                 maxCode = v
         return maxCode
-                
+
     def process_files(self):
+        "Crea una lista di comandi per le trasformazioni richieste"
         n = self.find_max_used_code() + 1
         results = []
         for (d, f) in self.src:
             if self.get_numeric_prefix(f) < 0:
-                results.append( (self.ACTION_RENAME, d, f, f"{self.to_num(n)}-{f}"))
-                n = n+ 1
+                results.append(
+                    (self.ACTION_RENAME, d, f, f"{self.to_num(n)}-{f}"))
+                n = n + 1
         return results
 
     @staticmethod
     def get_numeric_prefix(text: str) -> int:
         "Ottiene il prefisso numerico, o -1 se non presente"
         match = re.match(Rename.FILE_PREFIX, text)
-        
+
         if match:
-            return int(match.group(1))  
+            return int(match.group(1))
         else:
             return -1
-
-
 
     @staticmethod
     def remove_numeric_prefix(text: str) -> str:
         "Elimina il prefisso (se presente) dal nome del file"
         match = re.match(Rename.FILE_PREFIX, text)
-        
+
         if match:
             return match.group(2)  # Restituisce tutto dopo il prefisso
         else:
             return text  # Restituisce la stringa originale
-    
+
     @staticmethod
     def to_num(n: int) -> str:
         """
         Converte un intero (es. 42) in una stringa di 4 caratteri con zeri iniziali (es. '0042').
         """
         if n < 0 or n > 9999:
-            raise ValueError("Il numero deve essere compreso tra 0 e 9999 per la formattazione a 4 cifre.")
-            
+            raise ValueError(
+                "Il numero deve essere compreso tra 0 e 9999 per la formattazione a 4 cifre.")
+
         return f"{n:04d}"
-
-
--- toy-music.py
+++ toy-music.py
@@ -1,14 +1,38 @@
+"""
+The command toy-music.py
+"""
+
 import argparse
 from pathlib import Path
 from rename import Rename
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    "Main entry point"
+    parser = argparse.ArgumentParser(
+        prog="toy-music.py",
+        description="""
+Rewrites a set of mp3s to be played in sequence on Toyota audio players.
+
+To do this, it prefixes each file with an incremental code, e.g.
+myfile.mp3 becomes  0001-myfile.mp3
+
+Files that are already prefixed won't be changed; and new files
+will be prefixed after them.
+
+""",
+        epilog="""
+Usage:
+
+  %(prog)s /Volumes/DRIVE           # prints changes
+  %(prog)s --run /Volumes/DRIVE     # actually applies changes
+    """,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
     parser.add_argument("dir",
-                        help="display a square of a given number",
+                        help="The folder to process",
                         type=Path)
-    parser.add_argument("--run", 
+    parser.add_argument("--run",
                         help="Actually makes changes",
                         action="store_true")
     args = parser.parse_args()
@@ -25,7 +49,9 @@ def main():
     if (not args.run):
         print("\n*** No changes made - to make changes, run with --run flag")
 
+
 def apply_change(cmd, do_it):
+    "This executes cmmands created by Rename"
     print(f"- {cmd}")
     if (do_it):
         if cmd[0] == Rename.ACTION_RENAME:
